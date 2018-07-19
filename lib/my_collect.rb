b3e28e2804547934f7622b936868bded7387a261
Angie Kassab <anasab12@gmail.com>
2018-07-19 22:58:53 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,17 @@ def my_collect(collection)
   i = 0
   new_array_modified_collection = []
   while i < collection.count
+<<<<<<< HEAD
     new_array_modified_collection << yield(collection[i])
     i += 1
   end
   new_array_modified_collection
 end
+=======
+    new_array_modified_collection << yield(collection[0])
+    i += 1
+  end
+  new_array_modified_collection
+end
+
+>>>>>>> dce3d1a76e6276f782e57860c46e88ad12ee8781
